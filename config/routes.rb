c58ecb0fc4ccea32476955003a7e2790e16816a6
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-07 12:27:15 -0300

    feat: define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  get "movie/:id", to: "movie#index"
+
+  resources :movies, only: [ :new, :create, :edit, :update, :destroy ]
+  resources :directors, only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
+  resources :genres, only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
 end
