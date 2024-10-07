92204344ac0d001f330218f06d4928853f9b8e9b
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-07 13:37:31 -0300

    feat: add show page to movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  resources :movies, only: [ :new, :create, :edit, :update, :destroy ]
+  resources :movies, only: [ :show, :new, :create, :edit, :update, :destroy ]
   resources :directors, only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
   resources :genres, only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
 end
