780729e24396f39a74e8e2d4d9082b7272b7456e
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-10 16:19:06 -0300

    feat: add edit button to director page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -21,6 +21,20 @@ class DirectorsController < ApplicationController
     @director = Director.find(params[:id])
   end
 
+  def edit
+    @director = Director.find(params[:id])
+  end
+
+  def update
+    @director = Director.find(params[:id])
+
+    if @director.update(director_params)
+      return redirect_to director_path
+    end
+
+    render :edit
+  end
+
   private
 
   def director_params
