5c53d3ebd1e612aa04dd42b46b019300a3e103f1
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-10 16:22:04 -0300

    feat: add edit button to genre page

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -17,8 +17,23 @@ class GenresController < ApplicationController
     render :new
   end
 
+
   def show
     @genre = Genre.find(params[:id])
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+
+    if @genre.update(genre_params)
+      return redirect_to genre_path
+    end
+
+    render :edit
   end
 
   private
